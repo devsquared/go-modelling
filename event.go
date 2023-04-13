7bdf0e695ffa0bb210c1ee9be59c40bae2c21ad0
Devin Ward <devinw1325wc@gmail.com>
2023-04-12 20:48:13 -0600

    builder and example update

--- event.go
+++ event.go
@@ -9,7 +9,6 @@ type Events map[EventIdentifier]StateIdentifier
 // Event defines something that happens to a state. For the sake of simplicity here, a State knows and defines the actions that
 // can happen to it. This is because the context of the original State is important for the outcome; not simply the action itself.
 type Event struct {
-	Identifier  EventIdentifier `json:"Identifier"`
-	Desc        string          `json:"Desc"`
-	ResultState StateIdentifier `json:"ResultState"`
+	Identifier EventIdentifier `json:"Identifier"`
+	Desc       string          `json:"Desc"`
 }
--- examples/example.go
+++ examples/example.go
@@ -10,17 +10,26 @@ type LightPower string
 const LightOn LightPower = "on"
 const LightOff LightPower = "off"
 
-// State is a basic example of a struct implementing the MachineState interface.
-type State struct {
+// LightOnState is a basic example of a struct implementing the State interface.
+type LightOnState struct {
 	name        string
 	identifier  godel.StateIdentifier
 	desc        string
 	knownEvents godel.Events
-	content     LightPower // simple On or Off
+	content     LightPower
+}
+
+// LightOffState is another basic example.
+type LightOffState struct {
+	name        string
+	identifier  godel.StateIdentifier
+	desc        string
+	knownEvents godel.Events
+	content     LightPower
 }
 
 // OnEvent deals with the event that comes into the machine and affects the state. As events are known, add a handler here.
-func (s *State) OnEvent(event godel.Event) (godel.StateIdentifier, error) {
+func (s *LightOnState) OnEvent(event godel.Event) (godel.StateIdentifier, error) {
 	fmt.Println("received " + event.Identifier + " event")
 
 	// check knownEvents for the event needed
@@ -31,8 +40,23 @@ func (s *State) OnEvent(event godel.Event) (godel.StateIdentifier, error) {
 	}
 }
 
-func (s *State) Identify() string {
-	return string(s.identifier)
+func (s *LightOnState) Identify() godel.StateIdentifier {
+	return "lightOn"
+}
+
+func (s *LightOffState) OnEvent(event godel.Event) (godel.StateIdentifier, error) {
+	fmt.Println("received " + event.Identifier + " event")
+
+	// check knownEvents for the event needed
+	if resultState, exists := s.knownEvents[event.Identifier]; exists {
+		return resultState, nil
+	} else {
+		return "", godel.ErrEventNotDefined
+	}
+}
+
+func (s *LightOffState) Identify() godel.StateIdentifier {
+	return "lightOff"
 }
 
 func main() {
@@ -40,7 +64,7 @@ func main() {
 		Identifier: "flipSwitch",
 	}
 
-	lightOnState := State{
+	lightOnState := LightOnState{
 		name:       "light is on state",
 		identifier: "lightOn",
 		knownEvents: map[godel.EventIdentifier]godel.StateIdentifier{
@@ -49,7 +73,7 @@ func main() {
 		content: LightOn,
 	}
 
-	lightOffState := State{
+	lightOffState := LightOffState{
 		name:       "light is off state",
 		identifier: "lightOff",
 		knownEvents: map[godel.EventIdentifier]godel.StateIdentifier{
@@ -58,14 +82,11 @@ func main() {
 		content: LightOff,
 	}
 
-	exampleLightSwitchMachine := godel.StateMachine{
-		Name: "Simple Light Switch State Machine",
-		States: map[godel.StateIdentifier]godel.MachineState{
-			"lightOn":  &lightOnState,
-			"lightOff": &lightOffState,
-		},
-		CurrentState: &lightOffState,
-	}
+	exampleLightSwitchMachine := godel.NewStateMachineBuilder("Simple Light Switch State Machine", &lightOffState).
+		WithDesc("A simple state machine simulating a simple light switch!").
+		AddState("lightOn", &lightOnState).
+		AddState("lightOff", &lightOffState).
+		BuildStateMachine()
 
 	fmt.Println("We are starting in the " + exampleLightSwitchMachine.CurrentState.Identify())
 	fmt.Println("Sending the light switch flip event.")
