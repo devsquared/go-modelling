7273cd69dae5dff0b7f284a98372db713be4147e
Devin Ward <devinw1325wc@gmail.com>
2023-04-12 20:48:13 -0600

    builder and example update

--- machine.go
+++ machine.go
@@ -2,14 +2,14 @@ package godel
 
 import "fmt"
 
-// TODO: Need to create a good builder pattern here so that creating machine with well-defined states and events is easy and intuitive
+// Need a way to execute many, many instances of a machine to test the specifications.
 
 // StateMachine defines a simple state machine instance.
 type StateMachine struct {
-	Name         string       `json:"Name"`
-	Desc         string       `json:"Desc"`
-	States       States       `json:"States"`
-	CurrentState MachineState `json:"-"`
+	Name         string `json:"Name"`
+	Desc         string `json:"Desc"`
+	States       States `json:"States"`
+	CurrentState State  `json:"-"`
 }
 
 func (m *StateMachine) SendEvent(event Event) error {
@@ -26,4 +26,26 @@ func (m *StateMachine) SendEvent(event Event) error {
 	}
 }
 
-// Need a way to execute many, many instances of a machine to test the specifications.
+type StateMachineBuilder struct {
+	machine *StateMachine
+}
+
+func NewStateMachineBuilder(name string, currentState State) *StateMachineBuilder {
+	return &StateMachineBuilder{machine: &StateMachine{Name: name, States: make(States, 0), CurrentState: currentState}}
+}
+
+func (b *StateMachineBuilder) AddState(id StateIdentifier, state State) *StateMachineBuilder {
+	b.machine.States[id] = state // simple set. it is clear if you add multiple with same id
+
+	return b
+}
+
+func (b *StateMachineBuilder) WithDesc(desc string) *StateMachineBuilder {
+	b.machine.Desc = desc
+
+	return b
+}
+
+func (b *StateMachineBuilder) BuildStateMachine() *StateMachine {
+	return b.machine
+}
--- state.go
+++ state.go
@@ -4,10 +4,10 @@ package godel
 type StateIdentifier string
 
 // States defines the states that are registered.
-type States map[StateIdentifier]MachineState
+type States map[StateIdentifier]State
 
-// MachineState is the definition for a state in a machine. A state needs to react to events and that is defined in OnEvent.
-type MachineState interface {
-	OnEvent(event Event) (StateIdentifier, error)
-	Identify() string
+// State is the definition for a state in a machine. A state needs to react to events and that is defined in OnEvent.
+type State interface {
+	OnEvent(event Event) (StateIdentifier, error) // Define what happens whenever a state encounters an event.
+	Identify() StateIdentifier                    // We need a way to identify states.
 }
